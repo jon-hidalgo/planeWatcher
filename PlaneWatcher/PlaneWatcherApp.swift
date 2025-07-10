be083b6f41b3d89e434df62b23f1815527c8f8dc
jonborghini <73173113+jonborghini@users.noreply.github.com>
2025-07-10 13:45:57 +0200

    Initial Commit

---
+++ PlaneWatcher/PlaneWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaneWatcherApp.swift
+//  PlaneWatcher
+//
+//  Created by Jon Hidalgo on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaneWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaneWatcherTests/PlaneWatcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaneWatcherTests.swift
+//  PlaneWatcherTests
+//
+//  Created by Jon Hidalgo on 10/7/25.
+//
+
+import Testing
+@testable import PlaneWatcher
+
+struct PlaneWatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
